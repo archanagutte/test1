90a978f764c32e3d0d7991c8f26454524b5dc816
archanagutte <101163829+archanagutte@users.noreply.github.com>
2022-03-14 12:42:47 +0530

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -3,3 +3,9 @@ learning git and github with python
 python is simple and easy to use rather than other language 
 welcome to python programming
 welcome.py
+print ('welcome to python programming language')
+//following program find for given no is even
+
+if (num % 2)==0:
+  
+  print ("the number is even")
